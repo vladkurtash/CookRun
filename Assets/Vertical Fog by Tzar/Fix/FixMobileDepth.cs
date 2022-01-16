6450152673a61f2afa3a8d3eedd662e1d6605bef
vladkurtash <82370921+vladkurtash@users.noreply.github.com>
2022-01-16 16:28:42 +0200

    Packages

---
+++ Assets/Vertical Fog by Tzar/Fix/FixMobileDepth.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class FixMobileDepth : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+    }
+}
