772106622329322cf47688f7386274467ec03959
vladkurtash <82370921+vladkurtash@users.noreply.github.com>
2022-01-24 16:31:24 +0200

    Implement basic Root functionality for Player creation

--- Assets/_Project/Scripts/Root.cs
+++ Assets/_Project/Scripts/Root.cs
@@ -1,9 +1,56 @@
 using UnityEngine;
+using CookRun.Model;
+using CookRun.View;
+using CookRun.Presenter;
+using CookRun.Systems;
+using CookRun.Input;
+using CookRun.Core;
 
 namespace CookRun
 {
     public class Root : MonoBehaviour
     {
-        
+        [SerializeField] private PlayerRootView playerRootView;
+        [SerializeField] private SlidingArea slidingArea;
+        private PlayerPresenter playerPresenter = null;
+        private PlayerInputRouter playerInputRouter = null;
+
+        private void Awake()
+        {
+            playerRootView = FindObjectOfType<PlayerRootView>();
+            slidingArea = FindObjectOfType<SlidingArea>();
+
+            var model = new PlayerModel(PlayerConfigDataSO.Instance.Data);
+            var moveSystem = new PlayerMoveSystem(PlayerMoveSystemDataSO.Instance.Data, model);
+            var rotateSystem = new PlayerRotateSystem(PlayerRotateSystemDataSO.Instance.Data, model);
+            var movementSystem = new PlayerMovementSystem(moveSystem, rotateSystem);
+
+            playerPresenter = new PlayerPresenter(model, playerRootView, movementSystem);
+
+            playerInputRouter = new PlayerInputRouter(slidingArea, movementSystem);
+        }
+
+        private void OnEnable()
+        {
+            playerPresenter.OnEnable();
+            playerInputRouter.OnEnable();
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            playerPresenter.OnDisable();
+            playerInputRouter.OnEnable();
+        }
+
+        private void Update()
+        {
+            playerPresenter.UpdateLocal(Time.deltaTime);
+            playerInputRouter.UpdateLocal(Time.deltaTime);
+        }
     }
 }
