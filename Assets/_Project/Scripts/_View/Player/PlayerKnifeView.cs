163d954bcf442a36dda4dcd52a268c4db302b742
vladkurtash <82370921+vladkurtash@users.noreply.github.com>
2022-01-27 12:46:36 +0200

    Implement PlayerKnifeView

--- Assets/_Project/Scripts/_View/Player/PlayerKnifeView.cs
+++ Assets/_Project/Scripts/_View/Player/PlayerKnifeView.cs
@@ -3,8 +3,29 @@ using UnityEngine;
 
 namespace CookRun.View
 {
+    [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
     public class PlayerKnifeView : AView, ITriggerable
     {
-        public event Action<Collider> OnTriggerEnter;
+        public event Action<Collider> TriggerEnter;
+
+        private void Awake()
+        {   
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<BoxCollider>().isTrigger = true;
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            TriggerEnter?.Invoke(collider);
+        }
+
+#if UNITY_EDITOR
+        public void OnDrawGizmos()
+        {
+            EzySlice.Plane cuttingPlane = new EzySlice.Plane();
+            cuttingPlane.Compute(transform);
+            cuttingPlane.OnDebugDraw();
+        }
+#endif
     }
 }
